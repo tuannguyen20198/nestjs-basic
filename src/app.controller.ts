9e94b4caafe7be2d322cacdc3c061e0973f55425
tuannguyen20198 <lntt20198@gmail.com>
2024-07-25 21:58:27 +0700

    commit local

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,17 +1,19 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller,Post, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ConfigService } from '@nestjs/config';
+import { LocalAuthGuard } from './auth/local-auth.guard';
+
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService,private configService : ConfigService) {}
+  constructor(
+    private readonly appService: AppService,
+    private configService : ConfigService) 
+    {}
   
-  @Get()
-  @Render("home")
-  getHello(){
-    const message = this.appService.getHello();
-    return {
-      message
-    }
-    // return "this.appService.getHello()";
+  @UseGuards(LocalAuthGuard)
+  @Post("/login")
+  handleLogin(){
+    return  "Tuan"
   }
+
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { UsersService } from 'src/users/users.service';
 import { UsersModule } from 'src/users/users.module';
+import { LocalStrategy } from './passport/local.strategy';
+import { PassportModule } from '@nestjs/passport';
+import { AuthService } from './auth.service';
 
 @Module({
-  providers: [AuthService],
-  imports:[UsersModule]
+  imports:[UsersModule,PassportModule],
+  providers: [AuthService,LocalStrategy],
 })
 export class AuthModule {}

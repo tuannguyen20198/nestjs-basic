7eba60b0cba1f4126ca27bfce2d14e0df28fcbfa
tuannguyen20198 <lntt20198@gmail.com>
2024-07-27 09:49:33 +0700

    commit jwt

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,19 +1,22 @@
-import { Controller,Post, UseGuards } from '@nestjs/common';
+import { Controller,Post, UseGuards,Request } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ConfigService } from '@nestjs/config';
 import { LocalAuthGuard } from './auth/local-auth.guard';
+import { AuthService } from './auth/auth.service';
 
 @Controller()
 export class AppController {
   constructor(
     private readonly appService: AppService,
-    private configService : ConfigService) 
+    private configService : ConfigService,
+    private authService:AuthService
+    ) 
     {}
   
-  @UseGuards(LocalAuthGuard)
-  @Post("/login")
-  handleLogin(){
-    return  "Tuan"
-  }
+    @UseGuards(LocalAuthGuard)
+    @Post('/login')
+    async login(@Request() req) {
+      return this.authService.login(req.user);
+    }
 
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,27 @@
 import { Module } from '@nestjs/common';
-import { UsersModule } from 'src/users/users.module';
+import { UsersModule } from '../users/users.module';
 import { LocalStrategy } from './passport/local.strategy';
 import { PassportModule } from '@nestjs/passport';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports:[UsersModule,PassportModule],
-  providers: [AuthService,LocalStrategy],
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get('SECRET_KEY'),
+        signOptions: {
+            expiresIn: configService.get('EXPIRE'),
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  providers: [AuthService, LocalStrategy],
+  exports: [AuthService], // Export AuthService if needed in other modules
 })
 export class AuthModule {}

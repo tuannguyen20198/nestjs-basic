7e19b47b18713c981b57c81fa3c273515771c2b3
tuannguyen20198 <lntt20198@gmail.com>
2024-08-30 21:34:03 +0700

    commit pagination

--- src/companies/companies.service.ts
+++ src/companies/companies.service.ts
@@ -5,6 +5,7 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Company, CompanyDocument } from './schemas/company.schema';
 import { SoftDeleteModel } from 'soft-delete-plugin-mongoose';
 import { IUser } from 'src/users/users.interface';
+import aqp from 'api-query-params';
 
 @Injectable()
 export class CompaniesService {
@@ -21,8 +22,33 @@ export class CompaniesService {
     });
   }
 
-  findAll() {
-    return `This action returns all companies`;
+  async findAll(currentPage :number,limit:number,qs:string) {
+    const { filter, sort, population } = aqp(qs);
+    
+    delete filter.page;
+    delete filter.limit;
+    let offset = (+currentPage - 1) * (+limit);
+    let defaultLimit = +limit ? +limit : 10;
+
+    const totalItems = (await this.companyModel.find(filter)).length;
+    const totalPages = Math.ceil(totalItems / defaultLimit);
+
+    const result = await this.companyModel.find(filter)
+    .skip(offset)
+    .limit(defaultLimit)
+    .sort(sort as any)
+    .populate(population)
+    .exec();
+
+    return {
+      meta: {
+      current: currentPage, //trang hiện tại
+      pageSize: limit, //số lượng bản ghi đã lấy
+      pages: totalPages, //tổng số trang với điều kiện query
+      total: totalItems // tổng số phần tử (số bản ghi)
+      },
+      result //kết quả query
+      }
   }
 
   findOne(id: number) {

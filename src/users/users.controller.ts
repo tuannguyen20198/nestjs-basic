e361ed5b546a64a8fc5932e41235d69f1a0a8af0
tuannguyen20198 <lntt20198@gmail.com>
2024-09-12 20:14:59 +0700

    commit create update paginate

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,38 +6,48 @@ import {
   Patch,
   Param,
   Delete,
+  Query,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { Public, ResponseMessage, User } from 'src/decorator/customize';
+import { IUser } from './users.interface';
 
 @Controller('users') // => /users
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  create( @Body()  CreateUserDto:CreateUserDto) 
+  create( @Body()  CreateUserDto:CreateUserDto, @User() user:IUser) 
   {
-    return this.usersService.create(CreateUserDto);
+    return this.usersService.create(CreateUserDto,user);
   }
 
   @Get()
-  findAll() {
-    return this.usersService.findAll();
+  @ResponseMessage("Fetch user with paginate")
+  findAll(
+    @Query("page") currentPage:string,
+    @Query("page") limit:string,
+    @Query() qs:string,
+  ) {
+    return this.usersService.findAll(+currentPage,+limit,qs);
   }
-
+  @Public()
   @Get(':id')
+  @ResponseMessage("Fetch user by id")
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(id);
   }
 
   @Patch()
-  update(@Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update( updateUserDto);
+  @ResponseMessage("Update a User")
+  update(@Body() updateUserDto: UpdateUserDto, @User() user:IUser) {
+    return this.usersService.update( updateUserDto,user);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.usersService.remove(id);
+  remove(@Param('id') id: string,@User() user:IUser) {
+    return this.usersService.remove(id,user);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,16 +2,19 @@ import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateUserDto, RegisterUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectModel } from '@nestjs/mongoose';
-import { User, UserDocument } from './schemas/user.schema';
+import { User as UserM, UserDocument } from './schemas/user.schema';
 import mongoose, { Model } from 'mongoose';
 import { genSaltSync, hashSync } from 'bcryptjs';
 import { compareSync } from 'bcrypt';
 import { SoftDeleteModel } from 'soft-delete-plugin-mongoose';
+import { IUser } from './users.interface';
+import { User } from 'src/decorator/customize';
+import aqp from 'api-query-params';
 
 @Injectable()
 export class UsersService {
   constructor(
-    @InjectModel(User.name) 
+    @InjectModel(UserM.name) 
     private userModel: SoftDeleteModel<UserDocument>
     
   ) {}
@@ -19,26 +22,62 @@ export class UsersService {
     return hashSync(password,genSaltSync(10));
   }
 
-  async create(users:CreateUserDto){
-    const hashPassword = this.getHashPassword(users.password);
+  async create(createUserDto:CreateUserDto,@User() user:IUser){
+    const {name,email,password,age,gender,address,role,company} = createUserDto;
+    const hashPassword = await this.getHashPassword(password)
 
-    let user = await this.userModel.create({
-      email:users.email,
-      password:hashPassword,
-      name:users.name,
-    });
-    return user;
+    //add logic check email
+    // const isExist = await this.userModel.findOne({email});
+    // if (isExist) {
+    //   throw new BadRequestException(`Email:${email} đã tồn tại trên hệ thống.Vui lòng đăng nhập email khác`)
+    // }
+    const newUser = this.userModel.create({
+      name,email,
+      password:hashPassword,age,
+      gender,address,
+      role,company,
+      createdBy:{
+        _id:user._id,
+        email:user.email
+      }
+    })
+    return newUser
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async findAll(currentPage :number,limit:number,qs:string) {
+    const { filter, sort, population } = aqp(qs);
+    
+    delete filter.page;
+    delete filter.limit;
+    let offset = (+currentPage - 1) * (+limit);
+    let defaultLimit = +limit ? +limit : 10;
+
+    const totalItems = (await this.userModel.find(filter)).length;
+    const totalPages = Math.ceil(totalItems / defaultLimit);
+
+    const result = await this.userModel.find(filter)
+    .skip(offset)
+    .limit(defaultLimit)
+    .sort(sort as any)
+    .populate(population)
+    .exec();
+
+    return {
+      meta: {
+      current: currentPage, //trang hiện tại
+      pageSize: limit, //số lượng bản ghi đã lấy
+      pages: totalPages, //tổng số trang với điều kiện query
+      total: totalItems // tổng số phần tử (số bản ghi)
+      },
+      result //kết quả query
+      }
   }
 
   findOne(id: string) {
       if (!mongoose.Types.ObjectId.isValid(id)) return `not found user`
       return this.userModel.findOne({
         _id:id
-      });
+      }).select("-password");
   }
 
   findOneByUsername(username: string) {
@@ -50,14 +89,29 @@ export class UsersService {
   isValidPassword(password,hashPassword){
     return compareSync(password,hashPassword);
   }
-  async update(updateUserDto: UpdateUserDto) {
-    return await this.userModel.updateOne({_id:updateUserDto._id},{...updateUserDto});
+  async update(updateUserDto: UpdateUserDto, @User() user:IUser) {
+    return await this.userModel.updateOne(
+      {_id:updateUserDto._id},
+      {
+        ...updateUserDto,
+        updatedBy:{
+          _id:user._id,
+          email:user.email
+        }
+      }
+    );
   }
 
-  remove(id: string) {
+  async remove(id: string,@User() user:IUser) {
     if (!mongoose.Types.ObjectId.isValid(id)) return `not found user`
+    await this.userModel.updateOne({
+      deletedBy:{
+        _id:user._id,
+        email:user.email
+      }
+    })
     return this.userModel.softDelete({
-      _id:id
+      _id:id,
     });
   }
   

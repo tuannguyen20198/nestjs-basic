e6c9704dbfb0ff20b1496e35ba5f2afe6c6d2bc4
tuannguyen20198 <lntt20198@gmail.com>
2024-10-01 05:59:38 +0700

    create life cycle

---
+++ src/databases/databases.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { DatabasesService } from './databases.service';
+import { DatabasesController } from './databases.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User } from 'src/decorator/customize';
+import { UserSchema } from 'src/users/schemas/user.schema';
+import { Permission, PermissionSchema } from 'src/permissions/schemas/permission.schema';
+import { Role, RoleSchema } from 'src/roles/schemas/role.schema';
+
+@Module({
+  controllers: [DatabasesController],
+  providers: [DatabasesService],
+  imports: [
+    MongooseModule.forFeature([
+      { name: User.name, schema: UserSchema },
+      { name: Permission.name, schema: PermissionSchema },
+      { name: Role.name, schema: RoleSchema },
+    ]),
+  ],
+})
+export class DatabasesModule {}
